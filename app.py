2e8cd2d8eb303d066330bffd9ac0db97ec971671
RaneemLoay <raneemloay123@gmail.com>
2024-06-18 19:08:16 +0300

    port added

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import re
@@ -116,4 +118,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    # app.run(debug=True)
