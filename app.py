993fdd84563bbd4ebc87a1bc6d60c4bd8083052b
Sohyla Tarek <88028324+Sohyla31@users.noreply.github.com>
2024-06-28 21:39:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,22 +21,24 @@ model_bilstm = None
 model_gru = None
 model_cnn = None
 model_svm = None
+tfidf_vectorizer = None
 
 def load_resources():
-    global tokenizer, tfidf_vectorizer, model_lstm, model_bilstm, model_gru, model_cnn ,model_svm
-    if tokenizer is None:
-        with open('./static/tokenizer.pkl', 'rb') as handle:
-            tokenizer = pickle.load(handle)
+    global tokenizer, tfidf_vectorizer, tfidf_vectorizer, model_lstm, model_bilstm, model_gru, model_cnn ,model_svm
     if tfidf_vectorizer is None:
-        tfidf_vectorizer = TfidfVectorizer()
-    if model_lstm is None:
-        model_lstm = load_model('./models/lstm_model.h5')
-    if model_bilstm is None:
-        model_bilstm = load_model('./models/bilstm_model.h5')
-    if model_gru is None:
-        model_gru = load_model('./models/gru_model.h5')
-    if model_cnn is None:
-        model_cnn = load_model('./models/cnn_model.h5')
+        with open('./static/tfidf_vectorizer.pkl', 'rb') as f:
+            tfidf_vectorizer = pickle.load(f)
+    # if tokenizer is None:
+    #     with open('./static/tokenizer.pkl', 'rb') as handle:
+    #         tokenizer = pickle.load(handle)
+    # if model_lstm is None:
+    #     model_lstm = load_model('./models/lstm_model.h5')
+    # if model_bilstm is None:
+    #     model_bilstm = load_model('./models/bilstm_model.h5')
+    # if model_gru is None:
+    #     model_gru = load_model('./models/gru_model.h5')
+    # if model_cnn is None:
+    #     model_cnn = load_model('./models/cnn_model.h5')
     if model_svm is None:
         model_svm = load('./models/svm_model.joblib')
 
@@ -59,21 +61,21 @@ def predict():
             processed_text = preprocess(text)
             print(processed_text)
 
-            # Tokenize and pad sequence
-            sequence = tokenizer.texts_to_sequences([processed_text])
-            padded_sequence = pad_sequences(sequence, maxlen=MAX_SEQUENCE_LENGTH)
-            padded_sequence = np.array(padded_sequence)
+            # # Tokenize and pad sequence
+            # sequence = tokenizer.texts_to_sequences([processed_text])
+            # padded_sequence = pad_sequences(sequence, maxlen=MAX_SEQUENCE_LENGTH)
+            # padded_sequence = np.array(padded_sequence)
 
-            # Make predictions using loaded models
-            pred_lstm = model_lstm.predict(padded_sequence)
-            pred_bilstm = model_bilstm.predict(padded_sequence)
-            pred_gru = model_gru.predict(padded_sequence)
-            pred_cnn = model_cnn.predict(padded_sequence)
+            # # Make predictions using loaded models
+            # pred_lstm = model_lstm.predict(padded_sequence)
+            # pred_bilstm = model_bilstm.predict(padded_sequence)
+            # pred_gru = model_gru.predict(padded_sequence)
+            # pred_cnn = model_cnn.predict(padded_sequence)
             
-            new_text_tfidf = tfidf_vectorizer.transform(processed_text)
+            new_text_tfidf = tfidf_vectorizer.transform([processed_text])
             pred_svm = model_svm.predict(new_text_tfidf)
-
-            predicted_class = "bullying" if pred_svm[0][0] < 0.5 else "not bullying"
+            predicted_class = "bullying" if pred_svm[0] > 0.5 else "not bullying"
+            # predicted_class = "bullying" if pred_svm[0][0] < 0.5 else "not bullying"
             print(f"text: {text}")
             print(f"prediction: {predicted_class}")
             return jsonify({'prediction': predicted_class})
